45ecb685d73937da389b287ac4a43554c03e24e3
root <root@ip-172-31-41-28.ec2.internal>
2020-04-22 19:00:06 +0000

    adding additional analysis options.

--- run_dlc.sh
+++ run_dlc.sh
@@ -30,6 +30,8 @@ outstore="ncapdata/localout/"
 accessdir "$userhome/$datastore" "$userhome/$outstore"
 
 ## Stereotyped download script for data. The only reason this comes after something custom is because we depend upon the AWS CLI and installed credentials. 
+echo $inputpath $bucketname $datastore
+
 download "$inputpath" "$bucketname" "$datastore"
 
 ## Stereotyped download script for config: 
@@ -37,29 +39,44 @@ download "$configpath" "$bucketname" "$datastore"
 
 ###############################################################################################
 ## parse the config to place model folder, myconfig_analysis, in the right places. 
-modelpath=$(cat "$userhome/$datastore/$configname" | jq '.modeldata.modelpath' | sed 's/"//g')
-configpath=$(cat "$userhome/$datastore/$configname" | jq '.modeldata.configpath' | sed 's/"//g')
+## First figure out if we're in training or test mode. 
+analysismode=$(cat "$userhome/$datastore/$configname" | jq '.mode' | sed 's/"//g')
+if [ $analysismode == "test" ] 
+then
+    echo in test branch. 
+    exit
+    modelpath=$(cat "$userhome/$datastore/$configname" | jq '.modeldata.modelpath' | sed 's/"//g')
+    configpath=$(cat "$userhome/$datastore/$configname" | jq '.modeldata.configpath' | sed 's/"//g')
 
-## place python myconfig_analysis file and modelfolder in the right location.
-aws s3 sync "s3://""$bucketname"/"$modelpath" "$userhome/DeepLabCut/pose-tensorflow/models/"$(basename "$modelpath")""
-aws s3 cp "s3://""$bucketname"/"$configpath" "$userhome/DeepLabCut/myconfig_analysis.py"
-## Replace the video location in the config folder. 
-python "ncap_remote/substitute_config.py"
+    echo $modelpath modelpath
+    echo $configpath configpath
 
-## Preprocess videos 
+    ## place python myconfig_analysis file and modelfolder in the right location.
+    aws s3 sync "s3://""$bucketname"/"$modelpath" "$userhome/DeepLabCut/pose-tensorflow/models/"$(basename "$modelpath")""
+    aws s3 cp "s3://""$bucketname"/"$configpath" "$userhome/DeepLabCut/myconfig_analysis.py"
+    ## Replace the video location in the config folder. 
+    python "ncap_remote/substitute_config.py"
 
-## Run deeplabcut analysis: 
-cd DeepLabCut/Analysis-tools
+    ## Preprocess videos 
 
-python AnalyzeVideos.py
-cd "$userhome"/"$datastore"
+    ## Run deeplabcut analysis: 
+    cd DeepLabCut/Analysis-tools
 
-find -iname "*.h5" -exec cp {} "$userhome"/"$outstore" \;
-find -iname "*.pickle" -exec cp {} "$userhome"/"$outstore" \;
+    python AnalyzeVideos.py
+    cd "$userhome"/"$datastore"
 
-## Copy:
-cd "$userhome"
+    find -iname "*.h5" -exec cp {} "$userhome"/"$outstore" \;
+    find -iname "*.pickle" -exec cp {} "$userhome"/"$outstore" \;
 
+    ## Copy:
+    cd "$userhome"
+elif [ $analysismode == "train" ]
+then 
+    echo training
+else
+    echo "Mode $analysismode not recognized. Valid options are train or test. Exiting"
+    exit
+fi 
 ###############################################################################################
 ## Stereotyped upload script for the data
 upload "$outstore" "$bucketname" "$groupdir" "$resultdir" "mp4"
