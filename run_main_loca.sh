da639f71548f2fc231e1438c93d65ae748e978d8
root <root@ip-172-31-82-94.ec2.internal>
2020-05-17 04:17:40 +0000

    loca runs.

--- run_main_loca.sh
+++ run_main_loca.sh
@@ -2,18 +2,25 @@
 ## Bash script that establishes ncap monitoring routines with minimal dependencies on other packages. 
 ## Load in helper functions. 
 execpath="$0"
-echo execpath
 scriptpath="$(dirname "$execpath")/ncap_utils"
+## Get in absolute path loader: 
+source "$scriptpath/paths.sh"
+
+## Now declare remote directory name for here and all sourced files: 
+set -a
+neurocaasrootdir=$(dirname $(get_abs_filename "$execpath"))
+set +a
 
 source "$scriptpath/workflow.sh"
 ## Import functions for data transfer 
 source "$scriptpath/transfer.sh"
 
-## Now parse arguments in to relevant variables: 
+## Now parse arguments in to relevant variables. These will be available to all scripts run from within. 
 # Bucket Name  $bucketname
 # Group Directory $groupdir 
 # Results Directory $resultdir
-# Dataset Name $dataname
+# Analysis Output Directory $process
+# Dataset Name (without path) $dataname
 # Dataset Full Path $datapath
 # Configuration Name # configname
 # Configuration Path # configpath
@@ -21,8 +28,15 @@ set -a
 parseargsstd "$1" "$2" "$3" "$4"
 set +a
 
-## Example usage:
-echo "$bucketname"/"$groupdir"/"$resultdir"/logs/DATASET_NAME:"$dataname"_STATUS.txt"" 
+echo $bucketname >> "/home/ubuntu/check_vars.txt" 
+echo $groudir >> "/home/ubuntu/check_vars.txt" 
+echo $resultdir >> "/home/ubuntu/check_vars.txt" 
+echo $processdir >> "/home/ubuntu/check_vars.txt" 
+echo $dataname >> "/home/ubuntu/check_vars.txt" 
+echo $datapath >> "/home/ubuntu/check_vars.txt" 
+echo $configname >> "/home/ubuntu/check_vars.txt" 
+echo $configpath >> "/home/ubuntu/check_vars.txt" 
+ 
 ## Set up Error Status Reporting:
 errorlog_init 
 
@@ -32,9 +46,15 @@ background_pid=$!
 echo $background_pid, "is the pid of the background process"
 
 ## MAIN SCRIPT GOES HERE #####################
-bash /home/ubuntu/bin/run.sh $bucketname $groupdir inputs $resultdir $resultdir/logs $dataname atlas.mat $configname 
+#bash wait 20 #   "$5" #/home/ubuntu/ncap_remote/run_yass.sh
+bash $neurocaasrootdir/locanmf/run.sh $bucketname $groupdir inputs $processdir $resultdir/logs $dataname atlas.mat $configname > "$neurocaasrootdir"/joboutput.txt 2>"$neurocaasrootdir"/joberror.txt
 ##############################################
-exit
+## Cleanup: figure out how the actual processing went. 
+## MUST BE RUN IMMEDIATELY AFTER PROCESSING SCRIPTS TO GET ERROR CODE CORRECTLY.
 errorlog_final
+
+## Once this is all over, send the config and end.txt file
+aws s3 cp s3://"$bucketname"/"$configpath" s3://"$bucketname"/"$groupdir"/"$processdir"/$configname
+aws s3 cp "$neurocaasrootdir"/end.txt s3://"$bucketname"/"$groupdir"/"$processdir"/
 kill "$background_pid"
 
