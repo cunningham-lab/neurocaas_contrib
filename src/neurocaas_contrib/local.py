2de6805d3c735d5d3c477c75eb283c3ca46de644
cellistigs <ta2507@columbia.edu>
2020-11-30 14:16:20 -0500

    developing source code to autoscript.

--- src/neurocaas_contrib/local.py
+++ src/neurocaas_contrib/local.py
@@ -11,7 +11,93 @@ import pathlib
 
 cfn_client = boto3.client("cloudformation")
 
-## 8/20.20 Sketch out a new class that will form the basis of the new python package.  
+## 11/23/20
+class NeuroCAASAutoScript(object):
+    """Developer tool to automate creation and testing of an analysis-specific bash script.   
+
+    """
+    def __init__(self,scriptjson,templatepath):
+        """
+        :param scriptjson: a json script that specifies all of the parameters necessary to set up a local analysis environment. Tells us to start a python environment check if we need to communicate to the gpu, as well as the locations to which we should load local data and write output. 
+        """
+        ## Check for all necessary arguments in the config file:
+        with open(scriptjson,"r") as f:
+            scriptdict = json.load(f)
+        args_req = {"script_name":str,"in":dict,"out":dict,"user_root_dir":str}
+        for ar in args_req: 
+            assert ar in scriptdict.keys()
+            assert type(scriptdict[ar]) == args_req[ar], "scriptdict is misformatted: type for '{}' is {}, should be {}".format(ar,type(ar),args_req[ar])
+        self.scriptdict = scriptdict
+
+        ## Load in data from template script:
+        with open(templatepath,"r") as f:
+            scriptlines = (f.readlines())
+        assert scriptlines[0].startswith("#!/bin/bash"), "We must initialize from and build a bash script."
+        self.scriptlines = scriptlines
+
+        ## Now we start doing setup: 
+        if "env_name" in scriptdict.keys() and scriptdict["env_name"] is not None:
+            self.add_conda_env()
+
+    def append_conda_path_command(self,path = None):
+        """Generates the material we want to append to the python path to find the anaconda environment correctly. Will assume that anaconda(3) is installed in the user's home directory. An alternative path to anaconda3/bin can be supplied if this is not the case.   
+        :param path: (optional) if given, will check that the anaconda bin exists at that location, instead of being installed in the user's root directory
+        :return: the bash command we will use to appropriately format the anaconda path. 
+        """
+        if path is None:
+            path = os.path.join(self.scriptdict["user_root_dir"],"anaconda3/bin")
+            
+
+        ## First check that this path exists and is appropriately formatted.
+        assert path.endswith("anaconda3/bin"), "Path must end with anaconda3/bin."
+        assert os.path.isdir(path),"The path {} does not exist. Please add/revise a manually added path to the anaconda bin."
+        
+        command = "export PATH=\"{}:$PATH\"".format(path)
+        return command
+
+    def check_conda_env(self,env_name):
+        """Checks if a conda env exists on this machine, and returns a boolean exists/not exists.
+
+        :param env_name: environment name. 
+        :return: boolean, if this environment exists or not. 
+        """
+
+        all_env_str = subprocess.check_output('conda env list',shell = True).decode("utf-8")
+        all_env_info = all_env_str.split("\n")[2:]
+        condition = any([env_info.startswith(env_name+" ") for env_info in all_env_info])
+
+        return condition 
+
+    def add_conda_env(self,check = True):
+        """Adds commands to enter a conda virtual environment to template script. If check, will check that this virtual environment exists before adding.  
+
+        """
+        env_name = self.scriptdict["env_name"]
+        if check:
+            assert self.check_conda_env(env_name),"conda environment must exist"
+
+        command = self.append_conda_path_command()+" \n"
+
+        #Now add the suggested lines to the script:  
+        self.scriptlines.append(command)
+
+    def write_new_script(self,filename):
+        """Writes the current contents of scriptlines to file. 
+
+        :param filename: name of file to write to. 
+        
+        """
+        with open(filename,"w") as f:
+            f.writelines(self.scriptlines)
+
+
+    def check_args():
+        """
+
+        """
+
+
+## 8/20/20 Sketch out a new class that will form the basis of the new python package.  
 class NeuroCAASDeveloperInterface(object):
     """New developer interface that will form the basis for a python package. 
 
@@ -23,7 +109,9 @@ class NeuroCAASDeveloperInterface(object):
         """
         try:
             response = cfn_client.describe_stacks(pipelinename)
-        TODO
+        #TODO
+        except:
+            pass
 
 
     def initialize_blueprint(instance_type,ami,region):
@@ -31,16 +119,17 @@ class NeuroCAASDeveloperInterface(object):
         Initializes a blueprint with the basic info needed to get an instance up and running.    
         ## Creates a special directory where pipeline specific information will be stored.  
         """
+        pass
 
     def load_blueprint(path):
         """
         If continuing development on a pipeline that already exists, you can load the information from it directly. 
         """
+        pass
 
     def launch_development_instance():
         """
         Launch a development instance from the blueprint you are building. 
         """
-
-
+        pass
 
