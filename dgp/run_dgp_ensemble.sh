49e8d47df142cfac30dbde65a1050791512c63ac
Ubuntu <ubuntu@ip-172-31-79-191.ec2.internal>
2021-02-21 02:31:08 +0000

    finalized script.

--- dgp/run_dgp_ensemble.sh
+++ dgp/run_dgp_ensemble.sh
@@ -37,12 +37,24 @@ task=$(neurocaas_contrib read-yaml -p "$userhome/$configstore/$configname" -f "t
 scorer=$(neurocaas_contrib read-yaml -p "$userhome/$configstore/$configname" -f "scorer")
 jobnb=$(neurocaas_contrib read-yaml -p "$userhome/$configstore/$configname" -f "jobnb")
 videotype=$(neurocaas_contrib read-yaml -p "$userhome/$configstore/$configname" -f "videotype")
+testing=$(neurocaas_contrib read-yaml -p "$userhome/$configstore/$configname" -f "testing")
+nb_frames=$(neurocaas_contrib read-yaml -p "$userhome/$configstore/$configname" -f "nb_frames" -d "")
+seed=$(neurocaas_contrib read-yaml -p "$userhome/$configstore/$configname" -f "seed" -d "")
 
 ## create project from raw data: 
-python neurocaas_ensembles/project_init.py "$task" "$scorer" "2030-01-0$jobnb" "$userhome/$datastore/" 
+python neurocaas_ensembles/project_init.py "$task" "$scorer" "2030-01-0$jobnb" "$userhome/$datastore/" "$nb_frames" "$seed"
 
 ## Run dgp: 
 cd "$userhome/deepgraphpose"
-python "demo/run_dgp_demo.py" --dlcpath "$userhome/$datastore/model_data/$task-$scorer-2030-01-0$jobnb/"
+if [ $testing == "True" ]
+then
+    python "demo/run_dgp_demo.py" --dlcpath "$userhome/$datastore/model_data/$task-$scorer-2030-01-0$jobnb/" --test
+elif [ $testing == "False" ]    
+then 	
+    python "demo/run_dgp_demo.py" --dlcpath "$userhome/$datastore/model_data/$task-$scorer-2030-01-0$jobnb/" 
+else    
+    echo "Mode $testing not recognized. Valid options are "True" or "False". Exiting."	
+    exit
+fi    
 
 aws s3 sync "$userhome/$datastore/model_data/$task-$scorer-2030-01-0$jobnb/" "s3://$bucketname/$groupdir/$processdir/$jobnb/"
