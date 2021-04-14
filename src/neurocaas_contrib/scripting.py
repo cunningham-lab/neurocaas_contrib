5dd997daf5c3235a924fc36cc95c0f54e587e341
Ubuntu <ubuntu@ip-172-31-79-191.ec2.internal>
2021-04-14 15:35:50 +0000

    added zip management function to contrib.

--- src/neurocaas_contrib/scripting.py
+++ src/neurocaas_contrib/scripting.py
@@ -1,6 +1,8 @@
 ## module for scripting tools. 
+import os
 import yaml
 import json
+import zipfile
 
 def get_yaml_field(yamlfile,fieldname):
     """returns the value of a field in a yaml file. 
@@ -10,7 +12,6 @@ def get_yaml_field(yamlfile,fieldname):
     """
     with open(yamlfile,"r") as f:
         ydict = yaml.full_load(f)
-        print(ydict,"ydict")
     try:    
         output = ydict[fieldname]
     except KeyError:    
@@ -21,4 +22,18 @@ def get_yaml_field(yamlfile,fieldname):
     else:
         return json.dumps(output)
 
+def parse_zipfile(zipname,path = None): 
+    """Given a zipfile, confirms that it is a zipfile, and that it contains one top level directory. Unzips the zip file, and returns the name of the top level directory. Will throw an error if 1) the file path is not a zip file, or 2) if it contains more than one top level directory. 
+
+    """
+    assert zipfile.is_zipfile(zipname), "File is not a recognized zip archive"
+    archive = zipfile.ZipFile(zipname)
+    full_namelist = archive.namelist()
+    folder = {item.split("/")[0] for item in full_namelist}.difference({'__MACOSX'})
+    filtered_namelist = [fn for fn in full_namelist if not fn.startswith('__MACOSX')]
+    assert len(folder) == 1; "Folder must contain only one top level directory." 
+    if path is None:
+        path = os.path.dirname(zipname)
+    archive.extractall(path = path,members = filtered_namelist)
+    return folder.pop()
 
