19a0fca3b176fd56dd3de094665889652b2b84ad
cellistigs <ta2507@columbia.edu>
2021-04-29 14:48:13 -0400

    tested and reformatted inputs for S3 upload/download.

--- src/neurocaas_contrib/Interface_S3.py
+++ src/neurocaas_contrib/Interface_S3.py
@@ -7,86 +7,93 @@ import boto3
 from boto3.s3.transfer import S3Transfer 
 import botocore 
 import threading
+
+s3 = boto3.resource('s3')
+s3_client = boto3.client("s3")
+
 ## from https://stackoverflow.com/questions/41827963/track-download-progress-of-s3-file-using-boto3-and-callbacks
 class ProgressPercentage_d(object):
-    def __init__(self,client,BUCKET,KEY):
+    """Helper class to get and display percentage of data downloaded. 
+
+    """
+    def __init__(self,client,BUCKET,KEY,display = False):
         self._filename = KEY
         self._size = client.head_object(Bucket=BUCKET,Key=KEY)['ContentLength']
         self._seen_so_far = 0
         self._lock = threading.Lock()
+        self.display = display
 
     def __call__(self,bytes_amount):
         with self._lock:
             self._seen_so_far += bytes_amount
             percentage = round((self._seen_so_far/self._size)*100,2)
-            #sys.stdout.write(
-            #            "\r%s  %s / %s  (%.2f%%)" % (
-            #            self._filename, self._seen_so_far, self._size,
-            #            percentage))
-            #sys.stdout.flush()
+            if self.display:
+                sys.stdout.write(
+                            "\r%s  %s / %s  (%.2f%%)" % (
+                            self._filename, self._seen_so_far, self._size,
+                            percentage))
+                sys.stdout.flush()
 
 class ProgressPercentage_u(object):
-    def __init__(self,FILEPATH):
+    """Helper class to get and display percentage of data uploaded. 
+
+    """
+    def __init__(self,FILEPATH,display = False):
         self._filename = FILEPATH
         self._size = float(os.path.getsize(FILEPATH)) 
         self._seen_so_far = 0
         self._lock = threading.Lock()
+        self.display = display
 
     def __call__(self,bytes_amount):
         with self._lock:
             self._seen_so_far += bytes_amount
             percentage = round((self._seen_so_far/self._size)*100,2)
-            #sys.stdout.write(
-            #            "\r%s  %s / %s  (%.2f%%)" % (
-            #            self._filename, self._seen_so_far, self._size,
-            #            percentage))
-            #sys.stdout.flush()
+            if self.display:
+                sys.stdout.write(
+                            "\r%s  %s / %s  (%.2f%%)" % (
+                            self._filename, self._seen_so_far, self._size,
+                            percentage))
+                sys.stdout.flush()
 
-def download(BUCKET_NAME,KEY,tempdir = '../vmnt/tmp_videos/'):
+def download(s3path,localpath,display = False):
+    """Download function. Takes an s3 path to an object, and local object path as input.   
+    :param s3path: full path to an object in s3. Assumes the s3://bucketname/key syntax. 
+    :param localpath: full path to the object name locally (i.e. with basename attached). 
+    :param display: (optional) Defaults to false. If true, displays a progress bar. 
 
-    s3 = boto3.resource('s3')
-    # for the purposes of temporary storage, we only use the last bit of the name as an indentifier: 
-    USEKEY = KEY.split('/')[-1]
+    """
+    assert s3path.startswith("s3://")
+    bucketname,keyname = s3path.split("s3://")[-1].split("/",1)
 
     try:
-        transfer = S3Transfer(boto3.client('s3','us-east-1')) 
-        progress = ProgressPercentage_d(transfer._manager._client,BUCKET_NAME,KEY)
-        transfer.download_file(BUCKET_NAME,KEY,tempdir+USEKEY,callback = progress)
+        transfer = S3Transfer(s3_client)
+        progress = ProgressPercentage_d(transfer._manager._client,bucketname,keyname,display = display)
+        transfer.download_file(bucketname,keyname,localpath,callback = progress)
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             print("The object does not exist.")
             raise
         else:
             raise
-def upload(BUCKET_NAME,FILENAME,FILEPATH,KEYPATH):
-    s3 = boto3.resource('s3')
-    
+
+def upload(localpath,s3path,display = False):
+    """Upload function. Takes a local object paht and s3 path to the desired key as input. 
+    :param localpath: full path to the object name locally (i.e. with basename attached). 
+    :param s3path: full path to an object in s3. Assumes the s3://bucketname/key syntax. 
+    :param display: (optional) Defaults to false. If true, displays a progress bar. 
+
+    """
+    assert s3path.startswith("s3://")
+    bucketname,keyname = s3path.split("s3://")[-1].split("/",1)
+
     try:
-        transfer = S3Transfer(boto3.client('s3','us-east-1'))
-        progress = ProgressPercentage_u(FILEPATH+FILENAME)
-        transfer.upload_file(FILEPATH+FILENAME,BUCKET_NAME,KEYPATH,callback = progress)
+        transfer = S3Transfer(s3_client)
+        progress = ProgressPercentage_u(localpath,display = display)
+        transfer.upload_file(localpath,bucketname,keyname,callback = progress)
 
     except OSError as e:
         print("The file does not exist.")
-        print(e)
-
-
-if __name__ == "__main__":
-    action = sys.argv[1]
-
-    bucket_name = 'froemkelab.videodata'
-    if action == 'download':
-        key = sys.argv[2]
-        download(bucket_name,key)
-    elif action == 'upload':
-        filename = sys.argv[2]
-        keypath = sys.argv[3]
-        upload(bucket_name,filename,keypath)
-    else:
-        print('not a valid action, please choose "upload" or "download"')
-
-
-
-
+        raise
 
 
--- tests/test_Interface_S3.py
+++ tests/test_Interface_S3.py
@@ -1,4 +1,58 @@
 import pytest
-import neurocaas_contrib.Interface_S3
+import json
+import os
+import localstack_client.session
+import neurocaas_contrib.Interface_S3 as Interface_S3
+
+@pytest.fixture
+def setup_simple_bucket(monkeypatch):
+    """Makes a simple bucket in localstack named testinterface with the following internal structure:  
+    s3://testinterface
+    |- user
+     |-file.json
+     |-config.json
+    """
+    bucketname = "testinterface"
+    username = "user"
+    contents = {
+            "file.json":{"data":"element"},
+            "config.json":{"param1":1}
+        }
+
+    session = localstack_client.session.Session()
+    s3_client = session.client("s3")
+    s3_resource = session.resource("s3")
+    monkeypatch.setattr(Interface_S3, "s3_client", session.client("s3")) ## TODO I don't think these are scoped correctly w/o a context manager.
+    monkeypatch.setattr(Interface_S3, "s3", session.resource("s3"))
+    s3_client.create_bucket(Bucket = bucketname)
+    for name,content in contents.items():
+        key = os.path.join(username,name)
+        writeobj = s3_resource.Object(bucketname,key)
+        content = bytes(json.dumps(content).encode("UTF-8"))
+        writeobj.put(Body = content)
+    return bucketname,username,contents,s3_client,s3_resource    
+
+def test_download(setup_simple_bucket,tmp_path):
+    download_loc = tmp_path / "downloc"
+    download_loc.mkdir()
+    bucket,username,contents,s3_client,s3_resource = setup_simple_bucket
+    s3path = f"s3://{bucket}/{username}/file.json"
+    Interface_S3.download(s3path,str(download_loc / "file.json"))
+    Interface_S3.download(s3path,str(download_loc / "file.json"),display = True)
+
+def test_upload(setup_simple_bucket,tmp_path):    
+    upload_loc = tmp_path / "uploc"
+    upload_loc.mkdir()
+    bucket,username,contents,s3_client,s3_resource = setup_simple_bucket
+    s3path = f"s3://{bucket}/{username}/up.json"
+    upload_file = str(os.path.join(upload_loc,"up.json"))
+    with open(upload_file,"w") as f:
+        json.dump({"up1":"item"},f)
+    Interface_S3.upload(upload_file,s3path)    
+    Interface_S3.upload(upload_file,s3path,display = True)    
+    Interface_S3.download(s3path,upload_file)
+        
+
+
 
 
