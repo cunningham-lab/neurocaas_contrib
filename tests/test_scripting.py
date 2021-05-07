af3c18cea187ddeba96fc196d638c98120d156dd
cellistigs <ta2507@columbia.edu>
2021-05-06 22:06:06 -0400

    committed changes to scripting.

--- tests/test_scripting.py
+++ tests/test_scripting.py
@@ -1,5 +1,6 @@
 import neurocaas_contrib.scripting as scripting
 import neurocaas_contrib.Interface_S3 as Interface_S3
+import neurocaas_contrib.log as log
 import localstack_client.session
 import shlex
 import json
@@ -18,16 +19,16 @@ def test_get_yaml_field(field,value):
         with pytest.raises(KeyError):
             output =scripting.get_yaml_field(test_yaml,field)
 
-@pytest.mark.parametrize("name,value",[(os.path.join(loc,"../../ensembledata_3/zz.zip"),"pass"),(os.path.join(loc,"../../ensembledata_3/1.zip"),"fail")])
+@pytest.mark.parametrize("name,value",[(os.path.join(loc,"test_mats","dir1.zip"),"pass"),(os.path.join(loc,"test_mats","dir2.zip"),"fail")])
 def test_parse_zipfile(name,value):
     zippath = name
     if value == "pass":
         folder = scripting.parse_zipfile(zippath)
-        assert folder == "Reaching-Mackenzie-2018-08-30"
+        assert folder == "dir1"
         assert os.path.exists(os.path.join(os.path.dirname(zippath),folder))
     elif value == "fail":
         folder = scripting.parse_zipfile(zippath)
-        assert folder == "1"
+        assert folder == "dir2"
         assert os.path.exists(os.path.join(os.path.dirname(zippath),folder))
 
 @pytest.fixture
@@ -40,9 +41,16 @@ def setup_full_bucket(monkeypatch):
          |-extra.json
        |-configs 
          |-config.json
+       |-results  
+         |-job__test
+           |-logs
+             |-certificate.txt
+             |-DATASET_NAME-file.json_STATUS.json
+             
     """
     bucketname = "testinterface"
     username = "user"
+    statusname = "DATASET_NAME-file.json_STATUS.txt.json"
     contents = {
             "inputs/file.json":{"data":"element"},
             "inputs/extra.json":{"extra":"info"},
@@ -54,12 +62,17 @@ def setup_full_bucket(monkeypatch):
     s3_resource = session.resource("s3")
     monkeypatch.setattr(Interface_S3, "s3_client", session.client("s3")) ## TODO I don't think these are scoped correctly w/o a context manager.
     monkeypatch.setattr(Interface_S3, "s3", session.resource("s3"))
+    monkeypatch.setattr(Interface_S3, "s3", session.resource("s3"))
+    monkeypatch.setattr(log, "s3_resource", session.resource("s3")) ## TODO I don't think these are scoped correctly w/o a context manager.
     s3_client.create_bucket(Bucket = bucketname)
     for name,content in contents.items():
         key = os.path.join(username,name)
         writeobj = s3_resource.Object(bucketname,key)
         content = bytes(json.dumps(content).encode("UTF-8"))
         writeobj.put(Body = content)
+    ## Now upload datastatus and certificate files:     
+    s3_client.upload_file(os.path.join(loc,"test_mats",statusname),bucketname,os.path.join("user","results","job__test","logs",statusname))    
+    s3_client.upload_file(os.path.join(loc,"test_mats","certificate.txt"),bucketname,os.path.join("user","results","job__test","logs","certificate.txt"))    
     return bucketname,username,contents,s3_client,s3_resource    
 
 def test_log_process():        
@@ -67,11 +80,12 @@ def test_log_process():
     goodscript = os.path.join(loc,"test_mats","sendtime.sh")
     logpath = os.path.join(loc,"test_mats","log")
 
-    brcode = scripting.log_process(shlex.split(badscript),"logpath","s3://fakepath")
-    gdcode = scripting.log_process(shlex.split(goodscript),"logpath","s3://fakepath")
+    brcode = scripting.log_process(shlex.split(badscript),"logpath","s3://fakepath/fakefile.txt")
+    gdcode = scripting.log_process(shlex.split(goodscript),"logpath","s3://fakepath/fakefile.txt")
     assert brcode == 127
     assert gdcode == 0
 
+@pytest.mark.skip
 def test_register_data():    
     s3datapath = "s3://bucketname/groupname/inputs/data.txt"
     scripting.register_data(s3datapath)
@@ -80,6 +94,7 @@ def test_register_data():
     assert z["datapath"] == s3datapath    
     os.remove("./.neurocaas_contrib_dataconfig.json")
 
+@pytest.mark.skip
 def test_register_config():    
     s3configpath = "s3://bucketname/groupname/configs/config.json"
     scripting.register_config(s3configpath)
@@ -88,6 +103,7 @@ def test_register_config():
     assert z["configpath"] == s3configpath    
     os.remove("./.neurocaas_contrib_dataconfig.json")
 
+@pytest.mark.skip
 @pytest.mark.parametrize("created",[["data"],["config"],["data","config"]])
 def test_get_dataset_name(created):    
     if "data" in created:
@@ -104,6 +120,7 @@ def test_get_dataset_name(created):
         assert data == os.path.basename(s3datapath)
     os.remove("./.neurocaas_contrib_dataconfig.json")
     
+@pytest.mark.xfail #Not used     
 @pytest.mark.parametrize("created",[["data"],["config"],["data","config"]])
 def test_get_config_name(created):    
     if "data" in created:
@@ -157,6 +174,16 @@ class Test_NeuroCAASScriptManager():
         with open(os.path.join(subdir,"registration.json"),"r") as fp:
             fi = json.load(fp)
         fi["additional_files"]["addfile"]["s3"] == "s3://bucket/groupname/inputs/filename.txt"    
+
+    def test_register_resultpath(self,tmp_path):    
+        subdir = tmp_path / "subdir"
+        subdir.mkdir()    
+        ncsm = scripting.NeuroCAASScriptManager(subdir)
+        ncsm.register_resultpath("s3://bucket/groupname/resuts/job__test/")
+
+        with open(os.path.join(subdir,"registration.json"),"r") as fp:
+            fi = json.load(fp)
+        assert fi["resultpath"] == "s3://bucket/groupname/resuts/job__test/"   
 
     def test_from_registration(self,tmp_path):
         subdir = tmp_path / "subdir"
@@ -316,7 +343,9 @@ class Test_NeuroCAASScriptManager():
             ncsm.get_file(name)
             assert ncsm.get_filepath(name) == os.path.join(subdir,"inputs",os.path.basename(contentkey))
 
-    def test_log_command(self,tmp_path):
+    @pytest.mark.parametrize("logging",("local","s3"))
+    def test_log_command(self,tmp_path,setup_full_bucket,logging):
+        bucketname,username,contents,s3_client,s3_resource = setup_full_bucket
         subdir = tmp_path / "subdir"
         subdir.mkdir()    
         ncsm = scripting.NeuroCAASScriptManager(subdir)
@@ -325,8 +354,36 @@ class Test_NeuroCAASScriptManager():
         goodscript = os.path.join(loc,"test_mats","sendtime.sh")
         logpath = os.path.join(loc,"test_mats","log")
 
-        brcode = ncsm.log_command(shlex.split(badscript),"s3://fakepath")
-        gdcode = ncsm.log_command(shlex.split(goodscript),"s3://fakepath")
-        assert "log.txt" in os.listdir(subdir / "logs")
+        if logging == "local":
+            brcode = ncsm.log_command(shlex.split(badscript),"s3://fake/fake/results/job__test/logs/DATASET_NAME-file.json_STATUS.txt.json")
+            gdcode = ncsm.log_command(shlex.split(goodscript),"s3://fake/fake/results/job__test/logs/DATASET_NAME-file.json_STATUS.txt.json")
+            assert "log.txt" in os.listdir(subdir / "logs")
+            assert "certificate.txt" in os.listdir(subdir / "logs")
+            with open(os.path.join(subdir,"logs","certificate.txt"),"r") as f:
+                lines = f.readlines()
+            print(lines)    
+            assert lines[2].startswith("DATANAME: dataset.ext | STATUS: SUCCESS")
+        if logging == "s3":
+            brcode = ncsm.log_command(shlex.split(badscript),f"s3://{bucketname}/{username}/results/job__test/logs/DATASET_NAME-file.json_STATUS.txt.json")
+            status = s3_client.download_file(bucketname,f"{username}/results/job__test/logs/DATASET_NAME-file.json_STATUS.txt.json",os.path.join(subdir,"status.json"))
+            certificate = s3_client.download_file(bucketname,f"{username}/results/job__test/logs/certificate.txt",os.path.join(subdir,"certificate.txt"))
+
+            with open(os.path.join(subdir,"certificate.txt"),"r") as f:
+                lines = f.readlines()
+            assert lines[2].startswith("DATANAME: dataset.ext | STATUS: FAILED")
+            assert lines[9].startswith("WARNING: this is a test certificate")
+            with open(os.path.join(subdir,"status.json"),"r") as f:
+                status = json.load(f) 
+            gdcode = ncsm.log_command(shlex.split(goodscript),f"s3://{bucketname}/{username}/results/job__test/logs/DATASET_NAME-file.json_STATUS.txt.json")
+            status = s3_client.download_file(bucketname,f"{username}/results/job__test/logs/DATASET_NAME-file.json_STATUS.txt.json",os.path.join(subdir,"status.json"))
+            certificate = s3_client.download_file(bucketname,f"{username}/results/job__test/logs/certificate.txt",os.path.join(subdir,"certificate.txt"))
+
+            with open(os.path.join(subdir,"certificate.txt"),"r") as f:
+                lines = f.readlines()
+            assert lines[2].startswith("DATANAME: dataset.ext | STATUS: SUCCESS")
+            assert lines[9].startswith("WARNING: this is a test certificate")
+            with open(os.path.join(subdir,"status.json"),"r") as f:
+                status = json.load(f) 
+
 
 
