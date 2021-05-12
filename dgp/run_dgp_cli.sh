2a65c99e5455b631257972296639e4e0b8655dbd
cellistigs <ta2507@columbia.edu>
2021-05-12 12:23:51 -0400

    added full conditional flags to dgp script. assumes that deepgraphpose has a predict script as well.

--- dgp/run_dgp_cli.sh
+++ dgp/run_dgp_cli.sh
@@ -11,10 +11,41 @@ datapath=$(neurocaas-contrib workflow get-datapath)
 configpath=$(neurocaas-contrib workflow get-configpath)
 taskname=$(neurocaas-contrib scripting parse-zip -z "$datapath")
 
+mode=$(neurocaas-contrib scripting read-yaml -p $configpath -f mode -d predict)
+debug=$(neurocaas-contrib scripting read-yaml -p $configpath -f testing -d False)
+
 python "/home/ubuntu/neurocaas_contrib/dgp/project_path.py" "$userhome/$datastore/$taskname/config.yaml"
 
 cd "$userhome/deepgraphpose"
-python "demo/run_dgp_demo.py" --dlcpath "$userhome/$datastore/$taskname/"
-zip "/home/ubuntu/results.zip" "$userhome/$datastore/$taskname/videos_pred/"
+
+if [ $mode == "train" ]
+then
+    if [ $debug == "True" ]
+    then
+        python "demo/run_dgp_demo.py" --dlcpath "$userhome/$datastore/$taskname/" --test
+    elif [ $debug == "False" ]    
+    then 	
+        python "demo/run_dgp_demo.py" --dlcpath "$userhome/$datastore/$taskname/"
+    else    
+        echo "Debug setting $debug not recognized. Valid options are "True" or "False". Exiting."	
+        exit
+    zip "/home/ubuntu/results.zip" "$userhome/$datastore/$taskname/"
+    fi    
+elif [ $mode == "predict" ]    
+then
+    if [ $debug == "True" ]
+    then
+        python "demo/predict_dgp_demo.py" --dlcpath "$userhome/$datastore/$taskname/" --test
+    elif [ $debug == "False" ]    
+    then 	
+        python "demo/predict_dgp_demo.py" --dlcpath "$userhome/$datastore/$taskname/"
+    else    
+        echo "Debug setting $debug not recognized. Valid options are "True" or "False". Exiting."	
+        exit
+    zip "/home/ubuntu/results.zip" "$userhome/$datastore/$taskname/videos_pred/"
+    fi    
+else    
+    echo "Mode setting $mode not recognized. Valid options are "predict" or "train". Exiting."
+fi
+
 neurocaas-contrib workflow put-result -r "/home/ubuntu/results.zip" -d 
-#aws s3 sync "$userhome/$datastore/$taskname/videos_pred/" "s3://$bucketname/$groupdir/$processdir"
