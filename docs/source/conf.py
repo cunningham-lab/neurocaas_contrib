b67a764720d099ecb63860e4c6c56730ff9dd266
cellistigs <ta2507@columbia.edu>
2021-05-31 12:29:36 -0400

    added markdown parser.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'myst_parser',    
     'sphinx_click',    
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
