188e8ce04fded48596642639805f58037b3e536f
cellistigs <ta2507@columbia.edu>
2022-01-11 14:35:08 -0500

    added stub of fix in scripting.py file.

--- src/neurocaas_contrib/scripting.py
+++ src/neurocaas_contrib/scripting.py
@@ -96,6 +96,9 @@ def log_process(command,logpath,s3status):
     ncc.update_instance_info(updatedict)
     with io.open(logpath,"wb") as writer, io.open(logpath,"rb",1) as reader:
         starttime = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        if sys = "win":
+            raise not implementederror
+        else:    
         process = subprocess.Popen(command,stdout = writer,stderr = writer)
         ## initialize a legacy logging object. starttime
         sys.stdout.write("\n\n-------Start Process Log-------\n\n")
